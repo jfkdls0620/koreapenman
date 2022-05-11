64dc444e68b09da59dac818b75a3584cac0a6029
박정환 <19park@wholedoc.net>
2022-05-11 15:51:50 +0900

    change fontsize

--- extend/webfont.extend.php
+++ extend/webfont.extend.php
@@ -5,7 +5,7 @@ function web_font() {
     echo "
         <link rel=preconnect href=https://fonts.googleapis.com>
         <link rel=preconnect href=https://fonts.gstatic.com crossorigin>
-        <link href=https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap rel=stylesheet>
-        <style>div, span, table, p, input, textarea, button, select, h1, h2, h3, a { font-family:'Nanum Myeongjo' !important; }</style>
+        <link href=https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap rel=stylesheet>
+        <style>div, span, table, p, input, textarea, button, select, h1, h2, h3, a { font-family:'Noto Sans KR' !important; }</style>
     ";
 }
